2f7d4234c373eddd5dfac25cb4f8b2c712f82539
Kevin Reiss <kevin.reiss@gmail.com>
2016-06-03 23:31:46 -0500

    CAP Setup and login redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
+  # go back to the url you were at after signing in
   def after_sign_in_path_for(_resource)
     request.env['omniauth.origin'] || root_path
   end
