639bb3aecfad8c611f3c439a4ebdc84c3f1d128c
Kevin Reiss <kevin.reiss@gmail.com>
2016-06-06 15:52:05 -0500

    SMTP for demo app.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'dss.princeton.edu'
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
